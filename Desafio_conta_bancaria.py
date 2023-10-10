997ca7e8d5d792412f0b8263955ea8617a8058de
Thiagoxdbs <thiago-hiago201@hotmail.com>
2023-10-10 11:21:07 -0300

    UTIMO ELSE

--- Desafio_conta_bancaria.py
+++ Desafio_conta_bancaria.py
@@ -1,5 +1,6 @@
 saldo = 0
 extrato = ''
+contsaq = 0
 
 while True:
   print('''MENU
@@ -15,23 +16,31 @@ while True:
       saldo += valor
       extrato += f'Deposito feito: \nR${valor:.2f}\n'
       print(f'Saldo:\n R$: {saldo}')
+      continue
     else:
       print('Operação falhou! O valor informado está invalido')
       continue
   elif menu in 'Ss':
     valor = float(input("Digite o valor que ira retirar: "))
-    if valor <= saldo and valor <= 500:
+    if valor <= saldo and valor <= 500 and contsaq < 3:
       saldo -= valor
       extrato += f'Saque de feito: \nR${valor:.2f}\n'
       print(f'Saldo:\n R$: {saldo}')
+      contsaq =+ 1
+      continue
+    elif contsaq == 3:
+      print("Já foram feitos três saques")
+      continue
     elif valor > 500:
       print('Operação falhou! Permitido tirar no maximo R$: 500,00')
+      continue
     else:
       print('Operação falhou! Conta não possuí o valor requerido!')
       continue
   elif menu in 'Ee':
     print(extrato)
     print(f'Saldo:\n R$: {saldo}')
+    continue
   elif menu in 'Qq':
     print('Obrigado pela preferência, volte sempre!')
     break
