df71c7aa4545c1bbe4190647c9386d767d74f3cc
Thiagoxdbs <thiago-hiago201@hotmail.com>
2023-10-18 15:47:12 -0300

    Iniciando Projeto 2.0, base

--- Desafio_conta_bancaria_2.0.py
+++ Desafio_conta_bancaria_2.0.py
@@ -1,1 +1,50 @@
-print("hello")
+saldo = 0
+extrato = ''
+contsaq = 0
+
+while True:
+  print('''MENU
+    [ D ]EPOSITAR
+    [ S ]ACAR
+    [ E ]XTRATO
+    [ Q ]AIR DO PROGRAMA''')
+  menu = (input('Escolha a opção no menu: '))
+  print('-'*20)
+  if menu in 'Dd':
+    valor = float(input("Digite o valor do depósito: "))
+    if valor > 0:
+      saldo += valor
+      extrato += f'Deposito feito: \nR${valor:.2f}\n'
+      print(f'Saldo:\n R$: {saldo}')
+      continue
+    else:
+      print('Operação falhou! O valor informado está invalido')
+      continue
+  elif menu in 'Ss':
+    contsaq =+ 1
+    valor = float(input("Digite o valor que ira retirar: "))
+    if contsaq < 3:
+      print("Já foram feitos três saques")
+      continue
+    elif valor <= saldo and valor <= 500 and contsaq < 3:
+      saldo -= valor
+      extrato += f'Saque de feito: \nR${valor:.2f}\n'
+      print(f'Saldo:\n R$: {saldo}')
+      continue
+    elif valor > 500:
+      print('Operação falhou! Permitido tirar no maximo R$: 500,00')
+      continue
+    else:
+      print('Operação falhou! Conta não possuí o valor requerido!')
+      continue
+  elif menu in 'Ee':
+    print(extrato)
+    print(f'Saldo:\n R$: {saldo}')
+    continue
+  elif menu in 'Qq':
+    print('Obrigado pela preferência, volte sempre!')
+    break
+  else:
+      print('Operação falhou! Digite uma opção valida!')
+      continue
+print('FIM DO PROGRAMA!')
